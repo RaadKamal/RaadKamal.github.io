10052aa2d80883e65fa270f9ac706890bdbd6d3e
RaadKamal <43869292+RaadKamal@users.noreply.github.com>
2025-08-26 12:58:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -473,7 +473,7 @@ ScrollReveal().reveal('.certificate', {
 
     duration: 2500, // Longer animation duration
 
-    delay: (el, i) => 500 + (i * 400), // Increased stagger delay by 400ms for each certificate
+    delay: (el, i) => 500 + (i * 200), // Increased stagger delay by 400ms for each certificate
 
     interval: 400, // Longer time between reveals
 
